87b5c2761bac0a53d823f95d7174bbf975902876
244075473 <244075473@qq.com>
2018-05-26 18:52:24 +0800

    主页

--- src/App.vue
+++ src/App.vue
@@ -11,34 +11,47 @@
         </div>
       </div>
     </nav>
-    <div class="container">
+    <div class="min-height">
       <router-view></router-view>
     </div>
+    <footer class="bottow-height">
+      <div class="container">
+        <p style="color: white; font-size: 16px; margin-top: 15px" class="text-center">Copyright © 2018 <strong>爱分享</strong>. All Rights Reserved.</p>
+        <p><a href="#" class="mr-2"  style="color: white; font-size: 16px">关于我们</a> <a href="mailto:admin@email.sectf.cn"  style="color: white; font-size: 16px">联系我们</a></p>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  data () {
-    return {
-      ProjectName: '爱分享',
-      IndexButton: '首页',
-      MyInfoButton: '我的',
-      PublishButton: '发布',
-      LoginButton: '登入'
+  export default {
+    name: 'App',
+    data () {
+      return {
+        ProjectName: '爱分享',
+        IndexButton: '首页',
+        MyInfoButton: '我的',
+        PublishButton: '发布',
+        LoginButton: '登入'
+      }
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 51px;
+  }
+  .min-height {
+    min-height: 600px;
+  }
+  .bottow-height{
+    height: 82px;
+    background-color: #737373;
+  }
 </style>
